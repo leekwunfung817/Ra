2b54a6492fb69911d138bdbb83f66018a4c98a64
leekwunfung817 <leekwunfung817@gmail.com>
2021-04-10 15:08:23 +0800

    Chromer driver

--- Script/Python/downloadWeb.py
+++ Script/Python/downloadWeb.py
@@ -3,8 +3,9 @@
 # python3 /Users/leekwunfung/Documents/GitHub/Ra/Script/Python/downloadWeb.py
 
 from selenium import webdriver
-from chromedriver_py import binary_path # this will get you the path variable
-
+# from chromedriver_py import binary_path # this will get you the path variable
+binary_path='/Users/leekwunfung/Documents/GitHub/Ra/Script/Python/chromedriver.89.0.4389.23'
 driver = webdriver.Chrome(executable_path=binary_path)
 driver.get("http://www.python.org")
 assert "Python" in driver.title
+# sleep(100)
