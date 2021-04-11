fd9569540642467671b71a19a3724e7ac544bde7
leekwunfung817 <leekwunfung817@gmail.com>
2021-04-11 22:08:56 +0800

    Resulting Result All

--- Script/Python/extractData_1.0.1.py
+++ Script/Python/extractData_1.0.1.py
@@ -15,10 +15,13 @@ def nextLine2sinLine(txt):
 
 path = "../../Data/His/ResultsStage_1.0.0/"
 Tpath = "../../Data/His/ResultsStage_1.0.1/"
+
+resultTxt = ''
 for file in os.listdir(path):
 	fp = path+file
 	print(fp)
 	fContent = open(fp,"r").read()
+
 	arr1 = []
 	arr = []
 	for line in fContent.split("\n"):
@@ -28,5 +31,17 @@ for file in os.listdir(path):
 			arr = []
 			continue
 		arr.append(line)
-	print(arr1,len(arr1))
-	break
+
+	full_txt = file+'\n'
+	for x in arr1[1:]:
+		txt="\t".join(x)
+		full_txt += txt+'\n'
+
+	resultTxt+=full_txt+'\n'
+
+	# print(full_txt)
+
+
+	# print(arr1,len(arr1))
+	# break
+open('../../Data/His/ResultsStage_1.0.1/result.txt','w+').write(resultTxt)
