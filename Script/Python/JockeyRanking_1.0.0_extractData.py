5131fa1ce77704ae0f835a26be54626b65f6bfaa
leekwunfung817 <leekwunfung817@gmail.com>
2021-04-11 23:27:15 +0800

    Jockey Rank

---
+++ Script/Python/JockeyRanking_1.0.0_extractData.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+
+# cd /Users/leekwunfung/Documents/GitHub/Ra/Script/Python/
+# python3 JockeyRanking_1.0.0_extractData.py
+
+import os
+
+def nextLine2sinLine(txt):
+	for x in range(1,10):
+		txt = txt.replace("\n\n\n", "\n\n")
+	return txt
+
+
+func = 'JockeyRanking'
+path = "../../Data/His/"+func+"/"
+Tpath = "../../Data/His/"+func+"_1.0.0/"
+for file in os.listdir(path):
+	if file.endswith(".html"):
+		fp = path+file
+		html_doc = open(fp,"r").read()
+		print(fp)
+		soup = BeautifulSoup(html_doc, 'html.parser')
+		arr = soup.select('table[class*="table_bd"]')
+		# print(arr,len(arr))
+		# i = 0 
+		for EachPart in arr:
+			txt = EachPart.get_text()
+			txt = txt.replace(' ','')
+			txt = txt.replace('騎師榜','')
+			txt = txt.replace('在港現役騎師','')
+			txt = txt.replace('按百分比顯示:','')
+			txt = nextLine2sinLine(txt)
+			
+			print(txt)
+		# 	i = i + 1
+			open(Tpath+file.replace('.html',''),'w+').write(txt)
---
+++ Script/Python/JockeyRanking_download.py
@@ -0,0 +1,54 @@
+
+# from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(800, 600))
+# display.start()
+
+# cd /Users/leekwunfung/Documents/GitHub/Ra/Script/Python/
+# python3 JockeyRanking_download.py
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--window-size=5,5")
+# chrome_options.add_argument("--no-startup-window")
+
+
+import os.path
+
+
+# from chromedriver_py import binary_path # this will get you the path variable
+
+
+
+def HisResAll(location):
+	binary_path='/Users/leekwunfung/Documents/GitHub/Ra/Script/Python/MacOS/chromedriver.89.0.4389.23'
+	url="https://racing.hkjc.com/racing/information/Chinese/Jockey/JockeyRanking.aspx?Season=Current&View=Numbers&Racecourse="+location
+	print(url)
+	# binary_path='/Users/leekwunfung/Documents/GitHub/Ra/Script/Python/MacOS/chromedriver.89.0.4389.23'
+	try:
+		driver = webdriver.Chrome(executable_path=binary_path,chrome_options=chrome_options)
+		driver.get(url)
+		time.sleep(5)
+		txt=driver.page_source
+		driver.close()
+		return txt
+	except Exception as e:
+		print(e.message)
+	return None
+
+def RememHis(location):
+	print(location)
+	funcName='JockeyRanking'
+	filename = "../../Data/His/"+funcName+"/"+location+".html"
+	if os.path.isfile(filename):
+		return
+	txt = HisResAll(location)
+	f = open(filename, "w+")
+	f.write(txt)
+	f.close()
+
+RememHis('ALL')
+RememHis('STT')
+RememHis('STA')
+RememHis('HVT')
