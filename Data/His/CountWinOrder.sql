1cfd5c8ab727e344179c2c8c96887f9864cf0b41
ivan.lee <ivan.lee@neuralt.com>
2021-04-22 18:51:56 +0800

    Update

--- Data/His/CountWinOrder.sql
+++ Data/His/CountWinOrder.sql
@@ -1,7 +1,15 @@
+What is 名次=WV?
+
 SELECT * from (
-SELECT avg(REPLACE(REPLACE(名次,'平頭馬',''),' ','')) o,馬名,count(*) c FROM LocalResults GROUP by 馬名 
+SELECT avg(REPLACE(REPLACE(名次,'平頭馬',''),' ','')) o,馬名,count(*) c FROM LocalResults GROUP by 馬名 
 )
 order by o asc;
 
-select * from LocalResults where
-馬名='不可擋(E336)';
+select 
+	avg(REPLACE(REPLACE(名次,'平頭馬',''),' ','')) o
+	,* 
+from LocalResults where
+馬名='何澤堯';
+
+4
+select * from LocalResults where 名次 like '% %';
