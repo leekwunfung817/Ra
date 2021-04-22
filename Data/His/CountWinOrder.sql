5205299b3a98eb8692588af8871591cda30b7dfa
leekwunfung817 <leekwunfung817@gmail.com>
2021-04-22 23:26:55 +0800

    Update three basic win posibilities factors

--- Data/His/CountWinOrder.sql
+++ Data/His/CountWinOrder.sql
@@ -1,15 +1,35 @@
 What is 名次=WV?
 
-SELECT * from (
-SELECT avg(REPLACE(REPLACE(名次,'平頭馬',''),' ','')) o,馬名,count(*) c FROM LocalResults GROUP by 馬名 
+SELECT avg(o) o,hn,count(*) c from 
+(
+	SELECT 
+		CAST(名次 AS INTEGER) o,
+		馬名 hn
+	FROM LocalResults 
 )
+where o>0
+group by hn
 order by o asc;
 
-select 
-	avg(REPLACE(REPLACE(名次,'平頭馬',''),' ','')) o
-	,* 
-from LocalResults where
-馬名='何澤堯';
+SELECT avg(o) o,n,count(*) c from 
+(
+	SELECT 
+		CAST(名次 AS INTEGER) o,
+		騎師 n
+	FROM LocalResults 
+)
+where o>0
+group by n
+order by o asc;
 
-4
-select * from LocalResults where 名次 like '% %';
+SELECT avg(o) o,n,count(*) c from 
+(
+	SELECT 
+		CAST(名次 AS INTEGER) o,
+		練馬師 n
+	FROM LocalResults 
+)
+where o>0
+group by n
+order by o asc;
+
