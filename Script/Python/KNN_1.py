147f322cb8cdf161e8d281eab079c9626ab96708
ivan.lee <ivan.lee@neuralt.com>
2021-05-12 17:18:21 +0800

    Completed the training of KNN

--- Script/Python/KNN_1.py
+++ Script/Python/KNN_1.py
@@ -15,7 +15,9 @@ python3 KNN_1.py
 from sklearn.neighbors import KNeighborsRegressor
 
 neigh = KNeighborsRegressor(n_neighbors=2)
+print(len(data),len(result))
 neigh.fit(data, result)
 # KNeighborsRegressor(...)
-print(neigh.predict([[1.5]]))
+predict = neigh.predict([[0.0,0.749283667621777,0.759206798866856,0.0404040404040404,0.571428571428571,0.318360002162948,0.823556088769317,0.573395293377875]])
+print(predict)
 # [0.5]
