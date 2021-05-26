104e2de19b21cf763d22f4ebea0248c89359da1e
ivan.lee <ivan.lee@neuralt.com>
2021-05-26 17:54:43 +0800

    Add the service dead loop version

--- Script/Python/LocalResults.cmd
+++ Script/Python/LocalResults.cmd
@@ -1,5 +1,7 @@
-
+:loop
 python LocalResults_download.py
 python LocalResults_1.0.0_extractData.py
 python LocalResults_1.0.1_extractData.py
 python LocalResults_1.0.1a_toSQL.py
+timeout 86400
+goto loop
--- Script/Python/RaceCard.cmd
+++ Script/Python/RaceCard.cmd
@@ -1,2 +1,5 @@
+:loop
 python3 RaceCard_download.py
 python3 RaceCard_1.0.0.py
+timeout 86400
+goto loop
